d1ac45dc5b5f232613313cb3fd09f760fd2400f4
Slava Mryasov <slavadevbox1@gmail.com>
2025-06-23 19:52:29 +0300

    N-4 chore: add fsd structure and aliases

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-[48px] text-green-500">Hello</div>
-}
-
-export default App
---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-[40px] text-green-500">Hello</div>
+}
+
+export default App
