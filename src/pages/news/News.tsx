a1fa1c82fc3cf1e6cc08b99fd587ac50d7d773d7
Slava Mryasov <slavadevbox1@gmail.com>
2025-06-29 13:00:29 +0300

    N-26 feat: create news page

--- src/pages/news/News.tsx
+++ src/pages/news/News.tsx
@@ -1,7 +1,18 @@
+import { NewsCardList } from '@/features/newsCardList'
+import { Carousel } from '@/features/newsCarousel'
+
 import { useGetNewsQuery } from '@/shared/api/newsApi'
+
+import { Spinner } from '@/shared/ui/spinner'
 
 export const News = () => {
   const { data: newsData, isLoading } = useGetNewsQuery()
-  console.log(newsData, isLoading)
-  return <div>News</div>
+
+  if (isLoading) return <Spinner />
+  return (
+    <>
+      <Carousel items={newsData?.data || []} />
+      <NewsCardList data={newsData?.data || []} />
+    </>
+  )
 }
